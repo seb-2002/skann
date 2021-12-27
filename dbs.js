ac56067341ffcace374e5a3d2435008c07c334e6
Sebastian Kann <sebastiankann@gmail.com>
2021-12-27 15:50:27 -0500

    first router

---
+++ dbs.js
@@ -0,0 +1,83 @@
+const db = {
+  en: {
+    menuItems: {
+      bio: {
+        title: "Bio",
+      },
+
+      perf: {
+        title: "Performance",
+        always_beautiful: {
+          title: "Always / Beautiful",
+        },
+        topical_dance: {
+          title: "Topical Dance",
+        },
+      },
+
+      dturgy: {
+        title: "Writing/Dramaturgy",
+      },
+
+      pgogy: {
+        title: "Teaching",
+      },
+
+      agenda: {
+        title: "Dates",
+      },
+
+      web: {
+        title: "Links",
+      },
+
+      contact: {
+        title: "Contact",
+        address: "sebastiankann@gmail.com",
+      },
+    },
+  },
+
+  fr: {
+    menuItems: {
+      bio: {
+        title: "Bio",
+      },
+
+      perf: {
+        title: "Création",
+        always_beautiful: {
+          title: "Always / Beautiful",
+        },
+        topical_dance: {
+          title: "Topical Dance",
+        },
+      },
+
+      dturgy: {
+        title: "Écriture/Dramaturgie",
+      },
+
+      pgogy: {
+        title: "Pédagogie",
+      },
+
+      agenda: {
+        title: "Agenda",
+      },
+
+      web: {
+        title: "Liens",
+      },
+
+      contact: {
+        title: "Contact",
+        address: "sebastiankann@gmail.com",
+      },
+    },
+  },
+};
+
+module.exports = {
+  db,
+};
--- express_server.js
+++ express_server.js
@@ -1,35 +1,15 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
-const db = {
-  en: {
-    menuItems: {
-      Bio: "this is my bio",
-      Performance: "performances",
-      "Writing/Dramaturgy": "",
-      Teaching: "",
-      Dates: "",
-      Links: "",
-      Contact: "sebastiankann@gmail.com",
-    },
-  },
-  fr: {
-    menuItems: {
-      Bio: "this is my bio",
-      Création: "performances",
-      "Écriture/Dramaturgie": "",
-      Pédagogie: "",
-      Dates: "",
-      Liens: "",
-      Contact: "sebastiankann@gmail.com",
-    },
-  },
+app.set("view engine", "ejs");
+
+const { db } = require("./dbs");
+
+const urlDatabase = {
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
-
-app.get("/", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 app.listen(PORT, () => {
   console.log(`skann listening on port ${PORT}!`);
@@ -37,4 +17,59 @@ app.listen(PORT, () => {
 
 app.get("/urls.json", (req, res) => {
   res.json(db);
+});
+
+// app.get("/en", (req, res) => {
+//   const params = JSON.stringify(req.params);
+//   const templateVars = { menuItems: db.en.menuItems, params };
+//   res.render("root", templateVars);
+// });
+
+// app.get("/", (req, res) => {
+//   const params = JSON.stringify(req.params);
+//   const templateVars = { menuItems: db.fr.menuItems, params: params };
+//   res.render("root", templateVars);
+// });
+
+app.get("/:lang", (req, res) => {
+  const lang = req.params.lang;
+  const templateVars = {
+    menuItems: db[lang].menuItems,
+    lang,
+  };
+  if (lang) {
+    res.render("root", templateVars);
+  }
+});
+
+app.get("/en/:menuItem", (req, res) => {
+  const menuItem = req.params.menuItem;
+  const templateVars = {
+    category: db.en.menuItems[menuItem],
+  };
+  res.render("category_page", templateVars);
+});
+
+app.get("/en/perf/:perf", (req, res) => {
+  const perf = req.params.perf;
+  const templateVars = {
+    perfData: db.en.menuItems.perf[perf],
+  };
+  res.render("perfs", templateVars);
+});
+
+app.get("/fr/:menuItem", (req, res) => {
+  const menuItem = req.params.menuItem;
+  const templateVars = {
+    category: db.fr.menuItems[menuItem],
+  };
+  res.render("category_page", templateVars);
+});
+
+app.get("/fr/perf/:perf", (req, res) => {
+  const perf = req.params.perf;
+  const templateVars = {
+    perfData: db.fr.menuItems.perf[perf],
+  };
+  res.render("perfs", templateVars);
 });
