4aba4be893cefa01381161b231fb1f7332b919ae
Sebastian Kann <sebastiankann@gmail.com>
2021-12-28 21:02:04 -0500

    made static pages accessible

--- express_server.js
+++ express_server.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use('/static', express.static('public'));
 
 const { db } = require("./dbs");
 
