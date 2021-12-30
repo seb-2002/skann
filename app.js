8a0fd300b60835be742c1b1097b22076985be304
Sebastian Kann <sebastiankann@gmail.com>
2021-12-30 15:03:37 -0500

    implemented redirects from root directory based on server language

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const compression = require("compression");
 const helmet = require("helmet");
+const defaultLang = "en";
 
 const app = express();
 app.use(helmet());
@@ -12,6 +13,7 @@ app.use(compression()); //Compress all routes
 app.use("/static", express.static("public"));
 
 const { db } = require("./dbs");
+const { redirect } = require("express/lib/response");
 
 app.listen(PORT, () => {
   console.log(`skann listening on port ${PORT}!`);
@@ -21,12 +23,28 @@ app.get("/urls.json", (req, res) => {
   res.json(db);
 });
 
+app.get("/", (req, res) => {
+  //detect browser language
+  const langString = req.headers["accept-language"];
+  const lang = langString.substring(0, 2);
+  //serve the french page if fr
+  if (lang === "fr") {
+    res.redirect("/fr");
+  } else if (lang === "en") {
+    res.redirect("/en");
+  } else {
+    //else serve default lang
+    res.redirect(`/${defaultLang}`);
+  }
+});
+
 app.get("/:lang", (req, res) => {
   const lang = req.params.lang;
   const templateVars = {
     menuItems: db[lang].menuItems,
     lang,
   };
+
   if (lang) {
     res.render("root", templateVars);
   }
