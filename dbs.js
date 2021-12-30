72af44bd75ff6dd968a19cbe00025c09943ba5ba
Sebastian Kann <sebastiankann@gmail.com>
2021-12-29 21:30:24 -0500

    theory/practice page

--- dbs.js
+++ dbs.js
@@ -58,8 +58,64 @@ const db = {
       },
 
       dturgy: {
-        title: "Writing/Dramaturgy",
+        title: "Theory/Practice",
         ticker: "theory/practice",
+        year_2021: {
+          year: 2021,
+          writing: {
+            title: "Writing &c",
+            projects: {
+              caio: {
+                description:
+                  "“Critical approaches to improvisation in circus performance (and some examples): unpacking the ideological with a detour through dance studies” (forthcoming)",
+                href: "https://www.circusanditsothers.org/speakers-and-artists",
+              },
+              tohu: {
+                description:
+                  "Circus Dramaturgy in Canada: Problems of Technique” (forthcoming)",
+              },
+            },
+          },
+          dramaturgy: {
+            title: "Dramaturgical collaborations",
+            projects: {
+              chose_en_soi: {
+                description:
+                  "The Chita Project- Anna Kichtchenko and Pablo Prampano // 'La Chose en soi' (research/working title)",
+                href: "https://www.facebook.com/thechitaproject/",
+              },
+            },
+          },
+          performer: {
+            title: "Perfoming for others",
+            projects: {},
+          },
+        },
+        year_2020: {
+          year: 2020,
+          writing: {
+            title: "Writing &c",
+            projects: {
+              caio: {
+                description:
+                  "“Critical approaches to improvisation in circus performance (and some examples): unpacking the ideological with a detour through dance studies” (forthcoming)",
+                href: "https://www.circusanditsothers.org/speakers-and-artists",
+              },
+              tohu: {
+                description:
+                  "Circus Dramaturgy in Canada: Problems of Technique” (forthcoming)",
+              },
+            },
+          },
+          dramaturgy: {
+            title: "Dramaturgical collaborations",
+            projects: {},
+          },
+          performer: {
+            title: "Perfoming for others",
+            projects: {},
+          },
+        },
       },
 
       pgogy: {
