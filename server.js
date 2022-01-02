32084c321cfe562f82f5e0445adb9d8aa2396341
Sebastian Kann <sebastiankann@gmail.com>
2022-01-01 20:34:00 -0500

    implemented sessions"

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
 const express = require("express");
 const compression = require("compression");
 const helmet = require("helmet");
+const session = require("express-session");
 const defaultLang = "en";
-
-const app = express();
-app.use(helmet());
 const PORT = 8080;
 
-app.set("view engine", "ejs");
+const app = express();
 
-app.use(compression()); //Compress all routes
-app.use("/static", express.static("public"));
+const sess = {
+  // genid: function (req) {
+  //   return genuuid(); // use UUIDs for session IDs
+  // },
+
+  cookie: {},
+  secret: "circus",
+  resave: false,
+  saveUninitialized: true,
+};
 
 const { db } = require("./dbs");
 const { redirect } = require("express/lib/response");
+const { MemoryStore } = require("express-session");
+const req = require("express/lib/request");
 
-const browsingHistory = [];
+app.use(helmet());
+
+app.set("view engine", "ejs");
+// app.set("trust proxy", 1); // trust first proxy
+
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1); // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sess));
+
+app.use(compression()); //Compress all routes
+app.use("/static", express.static("public"));
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`skann listening on port ${PORT}!`);
@@ -38,27 +59,33 @@ app.get("/", (req, res) => {
     //else serve default lang
     res.redirect(`/${defaultLang}`);
   }
-  browsingHistory.push("/");
-  console.log(`Browsing history: ${browsingHistory}`);
 });
 
 app.get("/:lang", (req, res) => {
   const lang = req.params.lang;
   if (lang === "en" || lang === "fr") {
+    if (req.session.history) {
+      req.session.history.push(lang);
+    } else {
+      req.session.history = [lang];
+    }
+    console.log(`User history: ${JSON.stringify(req.session.history)}`);
     const templateVars = {
       menuItems: db[lang].menuItems,
       lang,
     };
     res.render("root", templateVars);
   }
-  if (lang !== "favicon.ico") {
-    browsingHistory.push(lang);
-    console.log(`Browsing history: ${browsingHistory}`);
-  }
 });
 
 app.get("/en/:menuItem", (req, res) => {
   const menuItem = req.params.menuItem;
+  if (req.session.history) {
+    req.session.history.push(`en/${menuItem}`);
+  } else {
+    req.session.history = [`en/${menuItem}`];
+  }
+  console.log(`User history: ${JSON.stringify(req.session.history)}`);
   const templateVars = {
     category: db.en.menuItems[menuItem],
   };
@@ -67,6 +94,12 @@ app.get("/en/:menuItem", (req, res) => {
 
 app.get("/en/perf/:perf", (req, res) => {
   const perf = req.params.perf;
+  if (req.session.history) {
+    req.session.history.push(`en/perf/${perf}`);
+  } else {
+    req.session.history = [`en/perf/${perf}`];
+  }
+  console.log(`User history: ${JSON.stringify(req.session.history)}`);
   const templateVars = {
     perfData: db.en.menuItems.perf[perf],
   };
@@ -75,6 +108,12 @@ app.get("/en/perf/:perf", (req, res) => {
 
 app.get("/fr/:menuItem", (req, res) => {
   const menuItem = req.params.menuItem;
+  if (req.session.history) {
+    req.session.history.push(`fr/${menuItem}`);
+  } else {
+    req.session.history = [`fr/${menuItem}`];
+  }
+  console.log(`User history: ${JSON.stringify(req.session.history)}`);
   const templateVars = {
     category: db.fr.menuItems[menuItem],
   };
@@ -83,6 +122,12 @@ app.get("/fr/:menuItem", (req, res) => {
 
 app.get("/fr/perf/:perf", (req, res) => {
   const perf = req.params.perf;
+  if (req.session.history) {
+    req.session.history.push(`fr/perf/${perf}`);
+  } else {
+    req.session.history = [`fr/perf/${perf}`];
+  }
+  console.log(`User history: ${JSON.stringify(req.session.history)}`);
   const templateVars = {
     perfData: db.fr.menuItems.perf[perf],
   };
