6f5db07589d0721220039c84ba4974bbdb2cc2e8
Sebastian Kann <sebastiankann@gmail.com>
2022-01-05 20:43:05 -0500

    new bio page

--- db/db_en/bio_en.js
+++ db/db_en/bio_en.js
@@ -5,27 +5,62 @@ const bio_en = {
  
     {
       p: "Sebastian Kann (he/him) is a performer, theorist, dramaturge and performance maker.",
-      href: "",
+     
     },
     {
       p: "A graduate of the National Circus School in Montréal, his practice has been shaped by formative encounters with circus and dance communities in both Europe and North America.",
-      href: "",
+    
     },
     {
-      p: "His most recent performance, Always / Beautiful (created in 2019 with Natalie Oleinik), asks questions about why and how to stage technical virtuosity in the context of exhausting, performance-oriented success culture. ",
-      href: "/en/works/always_beautiful",
+      p_1: "His most recent performance",
+      p_2:{
+        anchor: "Always / Beautiful",
+        href:" https://manorhouseforever.net/performances",
+      },
+      p_3: " (created in 2019 with Natalie Oleinik), asks questions about why and how to stage technical virtuosity in the context of exhausting, performance-oriented success culture. ",
     },
     {
-      p: "  Kann is active as a writer, most recently co-editing Thinking Through Circus (APE Books, 2020), a collection of circus writing to which he also contributed a chapter.",
+      p: "  Kann is active as a writer, most recently co-editing",
+      p_2: {
+      anchor: "Thinking Through Circus",
       href: "https://artpapereditions.org/product/book/bauke-lievens-quintijn-ketels-sebastian-kann-vincent-focquet-thinking-through-circus",
+     
+
+      } ,
+      p_3:  "(APE Books, 2020), a collection of circus writing to which he also contributed a chapter.",
     },
     {
-      p: "In his studio practice, he takes aim at collective fantasies of embodiment, autonomy, and spontaneity, in an attempt to make palpable some of the ways liberalism has rendered these concepts difficult, incoherent, or impossible.",
-      href: "/en/works/topical_dance",
+      p: "In his",
+      p_2: {
+        anchor: "current studio practice",
+        href:"/en/works/topical_dance",
+        },
+        p_3: " he takes aim at collective fantasies of embodiment, autonomy, and spontaneity, in an attempt to make palpable some of the ways liberalism has rendered these concepts difficult, incoherent, or impossible.",
     },
     {
-      p: "As a dramaturge, he works extensively with the independent circus scene in Québec, supporting the creative processes of Emile Pineault, François Bouvier, and the Chita Project (Anna Kichtchenko and Pablo Pramparo), among others.",
-      href: "/en/web",
+      p: "As a dramaturge, he works extensively with the",
+     p_2: {
+        anchor: "independent circus scene",
+        href:"/en/collab",
+        },
+        p_3: "supporting the creative processes of ",
+     p_4: {
+        anchor: "Emile Pineault,",
+        href:"www.emilepineault.com",
+        },
+     p_5: {
+        anchor: "François Bouvier",
+        href:"www.acrobatetoimeme.com",
+        },
+        p_6: "and",
+     p_7: {
+        anchor: "The Chita Project (Anna Kichtchenko and Pablo Prampano)",
+        href:"/en/collab",
+        },
+
+
+       p_8: ", among others.",
+        
     },
     {
       p: "He lives in Montréal / Tiohtiá:ke, on the expropriated land of the Kanien’kehá:ka (Haudenosaunee Confederacy.",
